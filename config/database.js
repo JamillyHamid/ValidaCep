818cc5575a36f74c8c0aa6cfd62cfe2f66f09be5
JamillyHamid <hamidjamilly28@edu.unifil.br>
2024-08-16 20:34:36 -0300

    Subindo mudanças

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 module.exports = {
     dialect:'postgres',
     host: 'localhost',
-    username: 'usuario',
-    password: 'senha',
+    username: 'postgres',
+    password: '0106',
     database:'apinode',
     define: {
         timestamps: true,
