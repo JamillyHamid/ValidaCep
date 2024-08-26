f7bd9b185ce8f538f3e32d75242cc4a02a0be840
JamillyHamid <hamidjamilly28@edu.unifil.br>
2024-08-26 09:19:40 -0300

    Atualizando código

---
+++ routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const EnderecoController = require('./controllers/enderecoController');
+
+const router = express.Router();
+
+router.post('/enderecos', EnderecoController.createEndereco);
+router.get('/enderecos', EnderecoController.getAllEndereco);
+router.get('/enderecos/:Id', EnderecoController.getEnderecosById);
+router.put('/enderecos/:Id', EnderecoController.updateEndereco);
+router.delete('/enderecos/:Id', EnderecoController.deleteEnsdereco);
+
+module.exports = router;
